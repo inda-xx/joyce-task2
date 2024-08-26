ff3f7a1eb06c1b9e2aaf29b78f4e8176cca851cc
Avid <107996421+HotFazz@users.noreply.github.com>
2024-08-27 01:56:31 +0200

    Update Enemy.java

--- gen_src/Enemy.java
+++ gen_src/Enemy.java
@@ -1,51 +1,53 @@
 class Enemy {
-    // Fields for the Enemy class
     private String enemyType;
     private int health;
     private int damageLevel;
+    private int xPosition;
+    private int yPosition;
 
-    // Constructor
     public Enemy(String enemyType, int initialHealth, int damageLevel) {
         this.enemyType = enemyType;
         this.health = initialHealth;
         this.damageLevel = damageLevel;
+        // Initialize enemy's position randomly
+        this.xPosition = (int) (Math.random() * 5);
+        this.yPosition = (int) (Math.random() * 5);
     }
 
-    // Getters and Setters
     public String getEnemyType() {
-        // Return the enemy's type
         return enemyType;
     }
 
     public void setEnemyType(String enemyType) {
-        // Set the enemy's type
         this.enemyType = enemyType;
     }
 
     public int getHealth() {
-        // Return the enemy's health
         return health;
     }
 
     public void setHealth(int health) {
-        // Set the enemy's health
         this.health = health;
     }
 
     public int getDamageLevel() {
-        // Return the enemy's damage level
         return damageLevel;
     }
 
     public void setDamageLevel(int damageLevel) {
-        // Set the enemy's damage level
         this.damageLevel = damageLevel;
     }
 
-    // Methods
     public void attack(Player player) {
-        // Logic for the enemy attacking a player
+        player.encounterEnemy(this.damageLevel);
+        System.out.println(enemyType + " attacked " + player.getPlayerName() + " and dealt " + damageLevel + " damage.");
+    }
+
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    public int getYPosition() {
+        return yPosition;
     }
 }
-
-// GameMap.java
