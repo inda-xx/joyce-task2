93a1ceb129a8a2b7daaa1c6bd85ed3794e3c23e2
Avid <107996421+HotFazz@users.noreply.github.com>
2024-08-27 03:14:01 +0200

    Update Indamon.java

--- gen_src/Indamon.java
+++ gen_src/Indamon.java
@@ -1,59 +1,89 @@
 class Indamon {
-    private String name;
-    private int hp;
-    private int attack;
-    private int defense;
-    private boolean fainted;
-    
-    public Indamon(String name, int hp, int attack, int defense) {
-        this.name = name;
-        this.hp = hp;
-        this.attack = attack;
-        this.defense = defense;
-        fainted = false;
+
+  // Indamon fields
+  private String name;
+  private int hp;
+  private int attack;
+  private int defense;
+  private boolean fainted;
+
+  // Indamon getters
+  public String getName() {
+    return this.name;
+  }
+  public int getHp() {
+    return this.hp;
+  }
+  public int getAttack() {
+    return this.attack;
+  }
+  public int getDefense() {
+    return this.defense;
+  }
+  public boolean isFainted() {
+    return this.fainted;
+  }
+
+  // Indamon setters
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setHp(int hp) {
+    this.hp = hp;
+  }
+  public void setAttack(int attack) {
+    this.attack = attack;
+  }
+  public void setDefense(int defense) {
+    this.defense = defense;
+  }
+  public void setFainted(boolean fainted) {
+    this.fainted = fainted;
+  }
+
+  // Indamon constructor
+  public Indamon(String name, int hp, int attack, int defense, boolean fainted) {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.fainted = fainted;    
+  }
+
+  // prints info about an Indamon
+  public void printInfo() {
+    System.out.println("INFO");
+    System.out.println("Name: " + this.getName());
+    System.out.println("Hp: " + this.getHp());
+    System.out.println("Attack: " + this.getAttack());
+    System.out.println("Defense: " + this.getDefense());
+    System.out.println("Fainted: " + this.isFainted());
+  } 
+
+  // attacks a given Indamon
+  public void attack(Indamon victim) {
+    // only allows living indamon to attack
+    if (this.isFainted() == false) {
+      // removes hp from victim based on attack of attacker and defense on victim
+      victim.setHp(
+        victim.getHp() - this.getAttack() / victim.getDefense()
+      );
+
+      // sets victim as fainted if hp <= 0
+      if (victim.getHp() <= 0) {
+        victim.setFainted(true);
+      }
     }
-    
-    // getters
-    public String getName() { return name; }
-    public int getHp() { return hp; }
-    public int getAttack() { return attack; }
-    public int getDefense() { return defense; }
-    public boolean getFainted() { return fainted; }
+  }
 
-    // setters
-    public void setName(String name) { this.name = name; }
-    public void setHp(int hp) { this.hp = hp; }
-    public void setAttack(int attack) { this.attack = attack; }
-    public void setDefense(int defense) { this.defense = defense; }
-    public void setFainted(boolean fainted) { this.fainted = fainted;}
+  public static void main(String[] args) {
+    Indamon glassey = new Indamon("Glassey", 10, 5, 5, false);
+    Indamon glassey2 = new Indamon("Glassey2", 11, 6, 7, false);
+    Indamon vilmer = new Indamon("Vilmer", 99, 99, 99, false);
 
-    // attack method
-    public void attackOponnent(Indamon target) {
-        int damage = attack / target.getDefense();
-        target.setHp(target.getHp() - damage);
-        if (target.getHp() <= 0) {
-            target.setFainted(true);
-        }
-    }
-
-    // print info method
-    public void printInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("HP: " + hp);
-        System.out.println("Attack: " + attack);
-        System.out.println("Defense: " + defense);
-        System.out.println("Fainted: " + fainted);
-    }   
-    
-    public static void main(String[] args) {
-        // create a new "Indamon" object
-        Indamon glassey = new Indamon("Glassey", 10, 5, 5);
-        Indamon siberov = new Indamon("Siberov", 10, 5, 5);
-        // print out the object's info
-        glassey.printInfo();
-        siberov.printInfo();
-        // call the attack method
-        glassey.attack(siberov);
-        siberov.printInfo();
-    } // end main method
+    // check if things work
+    glassey.printInfo();
+    vilmer.attack(glassey);
+    glassey.printInfo();
+  }
 }
