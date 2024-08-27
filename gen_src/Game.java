6a2e2470dafae83d60d4154bb88e46a651fe0756
Avid <107996421+HotFazz@users.noreply.github.com>
2024-08-27 02:11:47 +0200

    Update Game.java

--- gen_src/Game.java
+++ gen_src/Game.java
@@ -1,4 +1,4 @@
-class Game {
+public class Game {
     public static void main(String[] args) {
         // Create a new Hero with a name and initial coordinates
         Hero hero = new Hero("John", 0, 0);
